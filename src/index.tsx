fd508567abd4a3a78d21e22080c3b58a12079978
Tyler Thomas <tyleredwardthomas@gmail.com>
2024-11-19 21:27:55 -0500

    chore: hashrouter

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.min.css";
 
@@ -13,13 +13,13 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
-    <BrowserRouter
+    <HashRouter
       basename={
         process.env.REACT_APP_NODE_ENV === "production" ? "/convergent" : "/"
       }
     >
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
 
